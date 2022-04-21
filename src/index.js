531ec6775b02db9536b06a00756b9e3cc0454108
juanfelipeocampopinzon <juanfelipeocampopinzon@gmail.com>
2022-04-21 14:27:00 -0500

    funcion puntaje

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Hreset = () => { }
+
 const App = () => {
   const [ bueno, setLeft] = useState(0)
   const [neutral, setRight] = useState(0)
   const [malo, setMalo] = useState(0)
   const [total, settotal ] = useState(0)
   const [porciento, setpor ] = useState(0)
+  const [puntaje, setpun ] = useState(0)
   return (
     <div>
       <p>Reseñas</p>
@@ -19,11 +22,11 @@ const App = () => {
       <button onClick={() => setMalo(malo + 1)(settotal(total + 1))}>
         malo
       </button>
-      <button onClick = {() =>(setpor(100*(bueno/total)))}>%</button>
+      <button onClick = {() =>(setpor(100*(bueno/total)))(setpun((bueno-malo)/total))}>%</button>
       <p>Estadisticas
       </p>
       <p>bueno {bueno}</p> <p> neutral {neutral}
-      </p> <p>malo {malo}</p> <p>total {total}</p> <p>positivo {porciento}%</p>
+      </p> <p>malo {malo}</p> <p>total {total}</p> <p>puntaje {puntaje}</p> <p>positivo {porciento}%</p>
     </div>
   )
 }
